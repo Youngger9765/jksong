3c093a78d8d603e91efc357e3777bad07afe287c
purpleice9765 <purpleice9765@msn.com>
2015-10-12 22:22:55 +0800

    CRUD

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,47 @@
 class VotesController < ApplicationController
+
+  def index
+    @votes = Vote.all
+  end
+
+  def show
+    @vote = Vote.find(params[:id])
+    
+  end
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = Vote.new(vote_params)
+    @vote.save
+    flash[:notice] = "新增成功的訊息"
+    redirect_to votes_path
+  end
+
+  def edit
+    @vote = Vote.find(params[:id])
+  end
+
+  def update
+    @vote = Vote.find(params[:id])
+    
+    if @vote.update(vote_params)
+      flash[:notice] = "event was successfully updated"
+      redirect_to vote_path(@vote)
+    else
+      render "edit"
+    end
+  end
+
+
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:content, :result)
+  end
+
+
 end
