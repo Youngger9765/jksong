d1fce333a6bc9dd60be42b6b8677fb96c596ea24
purpleice9765 <purpleice9765@msn.com>
2015-10-26 12:45:12 +0800

    issue show Title modify decision modify

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,17 @@
 module IssuesHelper
+
+  def personal_decision
+
+
+
+    if @profile_issue.try(:decision) = "1"
+      "贊成"
+    elsif @profile_issue.try(:decision) = "-1"
+      "反對"
+    elsif @profile_issue.try(:decision) = "0"
+      "不表態"
+    end  
+  end
+
+
 end
