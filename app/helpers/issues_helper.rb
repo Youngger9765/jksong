5668c4eec9b2866e4c421d1a2116dbb7870bc9de
purpleice9765 <purpleice9765@msn.com>
2015-10-26 12:46:01 +0800

    fix bug

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -4,11 +4,11 @@ module IssuesHelper
 
 
 
-    if @profile_issue.try(:decision) = "1"
+    if @profile_issue.try(:decision) == "1"
       "贊成"
-    elsif @profile_issue.try(:decision) = "-1"
+    elsif @profile_issue.try(:decision) == "-1"
       "反對"
-    elsif @profile_issue.try(:decision) = "0"
+    elsif @profile_issue.try(:decision) == "0"
       "不表態"
     end  
   end
