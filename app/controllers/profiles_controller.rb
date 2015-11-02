2c35a7c26b9dd3bcfdfaa89fb5007dc783f01575
purpleice9765 <purpleice9765@msn.com>
2015-11-02 23:02:46 +0800

    fix bug

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,7 +10,6 @@ class ProfilesController < ApplicationController
 
   def update
     if @profile.update(profile_params)
-      raise
       flash[:notice] = "event was successfully updated"
       redirect_to profile_path(@profile)
     else
