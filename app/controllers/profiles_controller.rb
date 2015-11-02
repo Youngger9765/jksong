2bac21eb3d275e6f86926b45f352d58e1003048a
purpleice9765 <purpleice9765@msn.com>
2015-11-02 23:00:09 +0800

    fix bug

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,8 +23,19 @@ class ProfilesController < ApplicationController
   end
 
   def profile_legislators_ships
-    @similar_legislators = current_user.get_similar_legislators
+    county = params[:county]
+    @similar_legislators = current_user.get_similar_legislators(10,county)
     @categories = Category.all
+
+    @user_max_vote_number=0
+    @categories.each do |c|
+      @profile.category_score_max(c.english_name)
+      if @profile.category_score_max(c.english_name) > @user_max_vote_number
+        @user_max_vote_number = @profile.category_score_max(c.english_name)
+      end
+      
+    end
+
   end
 
   def registed_data
